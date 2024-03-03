aa835dea15e72fce64a1bc4c05e183fdbc7e371b
GithubKarn <12karnaditya@gmail.com>
2024-03-03 14:55:46 +0530

    Modifies JS

--- js/header.js
+++ js/header.js
@@ -3,40 +3,44 @@ let thumbnails = document.querySelectorAll('.thumbnail .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
+let intervalId = setInterval(nextSlide, 6000);
 
 let countLen = slides.length;
 let activeSlide = 0;
 
-function nextSlide(){
+function nextSlide() {
     activeSlide += 1;
-    if(activeSlide >= countLen){
-        activeSlide=0;
+    if (activeSlide >= countLen) {
+        activeSlide = 0;
     }
     slideShow();
 }
 
-next.onclick = nextSlide;
-
-
-prev.onclick = function(){
-    if(activeSlide <= 0 ){
-        activeSlide = countLen;
-    }
+function prevSlide() {
     activeSlide -= 1;
+    if (activeSlide < 0) {
+        activeSlide = countLen - 1;
+    }
     slideShow();
 }
 
-
-function slideShow(){
-    let oldSlide = document.querySelector('.slider .list .item.active');
-    let oldThumb = document.querySelector('.thumbnail .item.active');
-
-    oldSlide.classList.remove('active');
-    oldThumb.classList.remove('active');
-
+function slideShow() {
+    for (let i = 0; i < countLen; i++) {
+        slides[i].classList.remove('active');
+        thumbnails[i].classList.remove('active');
+    }
     slides[activeSlide].classList.add('active');
     thumbnails[activeSlide].classList.add('active');
-
 }
 
-setInterval(nextSlide,6000);
+next.onclick = function() {
+    clearInterval(intervalId);
+    nextSlide();
+    intervalId = setInterval(nextSlide, 6000); // restart interval
+};
+
+prev.onclick = function() {
+    clearInterval(intervalId);
+    prevSlide();
+    intervalId = setInterval(nextSlide, 6000); // restart interval
+};
